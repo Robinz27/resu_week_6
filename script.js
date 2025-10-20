c210a45afdec900f478ce2a1a728f08453f33c05
Rattanatrai 🤍 <mimoza27zx@gmail.com>
2025-10-20 19:21:44 +0700

    Update

---
+++ script.js
@@ -0,0 +1,15 @@
+const p = document.getElementById("info");
+
+function func_main() {
+    console.log("Hello, World!");
+    p.innerHTML = `
+        Name : Rattanatrai Losares <br>
+        Student ID : 68034768 <br>
+        Major : Software Development <br>
+        Year : 1 <br>
+        Age : 19 <br>
+        Birthday : 27 March 2549 <br>
+        Email : mimoza27zx@gmail.com <br>
+        Phone Number : 098-359-8801 <br>
+    `; 
+}
