58365ad45f1802ef5c0405f26d2007a755d82e82
Rattanatrai 🤍 <mimoza27zx@gmail.com>
2025-11-11 12:20:38 +0700

    update

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const p = document.getElementById("info");
+const bt = document.getElementById("bt");
 
 function func_main() {
     console.log("Hello, World!");
     p.innerHTML = `
-        Name : Rattanatrai Losares <br>
-        Student ID : 68034768 <br>
-        Major : Software Development <br>
-        Year : 1 <br>
-        Age : 19 <br>
-        Birthday : 27 March 2549 <br>
-        Email : mimoza27zx@gmail.com <br>
-        Phone Number : 098-359-8801 <br>
-    `; 
+        <b>Name</b> : Rattanatrai Losares <br>
+        <b>Student ID</b> : 68034768 <br>
+        <b>Major</b> : Software Development <br>
+        <b>Year</b> : 1 <br>
+        <b>Age</b> : 19 <br>
+        <b>Birthday</b> : 27 March 2549 <br>
+        <b>Email</b> : mimoza27zx@gmail.com <br>
+        <b>Phone Number</b> : 098-359-8801 <br>
+    `;
+    bt.innerHTML = "Translate"
 }
